eb24d971b110ced66fbb9aa9adac336523f00f6a
Felipe Passos <felipepassos08@gmail.com>
2021-11-05 18:12:26 -0300

    Criado a migration cidades

---
+++ database/migrations/2021_11_05_201919_create_cidades_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cidades', function (Blueprint $table) {
+            $table->id();   //criar uma chave primaria id
+            $table->string('nome', 100)->unique(); //campo nome de tamanho 100 e não se pode repetir.
+            $table->timestamps();   //opccional e gerenciado pelo Laravel
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cidades');
+    }
+}
